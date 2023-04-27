94a585f7b76db6ce9e524f4a4e2d4074acbafe61
Narziyev Shohruhmirzo <shoxanarziyev12@gmail.com>
2023-04-27 19:23:52 +0500

    shoxa

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function onScanSuccess(qrCodeMessage) {
+    if(qrCodeMessage=='Shoxa'){
+        playOn()
+    }else{
+        playOFF()
+    }
     document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
 }
 
@@ -9,3 +14,6 @@ function onScanError(errorMessage) {
 var html5QrcodeScanner = new Html5QrcodeScanner(
     "reader", { fps: 10, qrbox: 250 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+let playOn = () => new Audio("audio/on.mp3").play();
+let playOFF = () => new Audio("audio/off.mp3").play();
